7be547b872a28e0fc2dd1fd1724ae1cf7503ec9e
Christian Schlemmer <christianschlemmer78@gmail.com>
2026-05-29 08:21:59 +0200

    hands-on: add LoadingIndicator.js to App.js

--- hands-on/src/App.js
+++ hands-on/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Switch, Redirect, useLocation } from "react-router-dom";
-import VoteListPage from "./components/VoteListPage";
 import NotFoundPage from "./components/NotFoundPage";
-import LoginPage from "./components/LoginPage";
-import VoteComposerPage from "./components/VoteComposerPage";
 import { useLogin } from "./components/LoginProvider";
+import LoadingIndicator from "./components/LoadingIndicator";
+
+const VoteListPage = React.lazy(() => import("./components/VoteListPage"));
+const LoginPage = React.lazy(() => import("./components/LoginPage"));
+const VoteComposerPage = React.lazy(() => import("./components/VoteComposerPage"));
 
 export default function App() {
   const location = useLocation();
@@ -31,26 +33,27 @@ export default function App() {
 
       <div className="Main">
         <div className="Container">
-          <Switch>
-            <Route exact path="/">
-              <VoteListPage />
-            </Route>
+          <React.Suspense fallback={<LoadingIndicator/>}>
+            <Switch>
+              <Route exact path="/">
+                <VoteListPage />
+              </Route>
 
-            <Route path="/votes/:voteId">
-              <VoteListPage />
-            </Route>
+              <Route path="/votes/:voteId">
+                <VoteListPage />
+              </Route>
 
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            
-            <Route path="/compose">{voteComposerOrLogin()}</Route>
+              <Route path="/login">
+                <LoginPage />
+              </Route>
+              
+              <Route path="/compose">{voteComposerOrLogin()}</Route>
 
-            <Route>
-              <NotFoundPage />
-            </Route>
-          </Switch>
-
+              <Route>
+                <NotFoundPage />
+              </Route>
+            </Switch>
+          </React.Suspense>
         </div>
       </div>
     </div>
